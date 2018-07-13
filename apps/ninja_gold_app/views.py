b3e0cbd2314f5eaf88593b2357e0473e91f9505c
unknown <steelst0@gmail.com>
2018-07-12 17:12:25 -0700

    basic html is up

--- apps/ninja_gold_app/views.py
+++ apps/ninja_gold_app/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
+    if 'gold' not in request.session:
+        request.session['gold']=0
     return render(request,'ninja_gold_app/index.html')
