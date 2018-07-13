92723f0e5bf6df74c1f9329eb9e2ad38519af336
unknown <steelst0@gmail.com>
2018-07-12 17:52:46 -0700

    text output works but only single line

--- apps/ninja_gold_app/views.py
+++ apps/ninja_gold_app/views.py
@@ -28,10 +28,11 @@ def process_money(request, methods=['POST']):
         request.session['goldEarned'] = random.randint(-50,50)
         print("GOLDEARNED: ",request.session['goldEarned'])
     request.session['gold'] += request.session['goldEarned']
-    # if request.session['goldEarned'] > 0:
-        # request.session['activity']+="Earned "+str(request.session['goldEarned'])+" gold from the "+request.session['building']+"!  ("+str(datetime.datetime.now().strftime("%y/%m/%d %H:%M"))+")\n"
-    # else:
-        # request.session['activity']+="Entered a "+request.session['building']+" and lost "+str(request.session['goldEarned'])+" gold... Ouch...  ("+str(datetime.datetime.now().strftime("%y/%m/%d %H:%M"))+")\n"
+    print(request.session)
+    if request.session['goldEarned'] > 0:
+        request.session['activity']="Earned "+str(request.session['goldEarned'])+" gold from the "+request.session['building']+"!  ("+str(datetime.datetime.now().strftime("%y/%m/%d %H:%M"))+")\n"
+    else:
+        request.session['activity']="Entered a "+request.session['building']+" and lost "+str(request.session['goldEarned'])+" gold... Ouch...  ("+str(datetime.datetime.now().strftime("%y/%m/%d %H:%M"))+")\n"
     return redirect('/')
 
 # Easy way to reset our session data for testing
